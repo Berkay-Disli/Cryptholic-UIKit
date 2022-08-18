1179ae2c6e7d9b65bbc1939a10ceb619df156326
Berkay-Disli <berkaydisli.work@gmail.com>
2022-08-18 15:49:43 +0300

    LineChart

--- CryptholicUIKitVersion/DetailsPage/CoinDetailsViewController.swift
+++ CryptholicUIKitVersion/DetailsPage/CoinDetailsViewController.swift
@@ -6,18 +6,23 @@
 //
 
 import UIKit
+import Charts
 
-class CoinDetailsViewController: UIViewController {
+class CoinDetailsViewController: UIViewController, ChartViewDelegate {
+    
+    var lineChart = LineChartView()
     
     var coin = Coin(id: "", icon: "", name: "", symbol: "", rank: 0, price: 0, priceBtc: 0, volume: 0, marketCap: 0, availableSupply: 0, totalSupply: 0, priceChange1h: 0, priceChange1d: 0, priceChange1w: 0, websiteURL: "", twitterURL: "", exp: [""], contractAddress: "", decimals: 0, redditURL: "")
     private var selectedTimeZone: TimezoneRanges = .day
     
+    @IBOutlet weak var lineChartViewLabel: UIView!
+    @IBOutlet weak var iconImage: UIImageView!
     @IBOutlet weak var coinNameLabel: UILabel!
     @IBOutlet weak var coinPriceLabel: UILabel!
     @IBOutlet weak var priceChangeLabel: UILabel!
     
-    @IBOutlet weak var dayButton: UIButton!
     
+    @IBOutlet weak var dayButton: UIButton!
     @IBOutlet weak var weekButton: UIButton!
     @IBOutlet weak var monthButton: UIButton!
     @IBOutlet weak var threeMonthsButton: UIButton!
@@ -31,11 +36,74 @@ class CoinDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        lineChart.delegate = self
+        iconImage.setImage(imageUrl: coin.icon)
         coinNameLabel.text = coin.name
         coinPriceLabel.text = "\(coin.price.formatted())"
         priceChangeLabel.text = "\(coin.priceChange1d ?? 0)"
+        
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        lineChart.frame = lineChartViewLabel.frame
+        lineChart.center = lineChartViewLabel.center
+        lineChart.drawGridBackgroundEnabled = false
+        view.addSubview(lineChart)
+        getCoinDetailsData(coin: coin)
+        
     }
     
+    /*
+    func chartSettings() {
+        let set = LineChartDataSet(entries: [
+            ChartDataEntry(x: Double(0), y: Double(1)),
+            ChartDataEntry(x: Double(1), y: Double(4)),
+            ChartDataEntry(x: Double(2), y: Double(2)),
+            ChartDataEntry(x: Double(3), y: Double(8)),
+            ChartDataEntry(x: Double(4), y: Double(5))
+        ])
+        set.colors = ChartColorTemplates.material()
+        let data = LineChartData(dataSet: set)
+        
+        
+        lineChart.data = data
+    }
+     */
+    
+    func getCoinDetailsData(coin: Coin) {
+            guard let url = URL(string: "https://api.coinstats.app/public/v1/charts?period=24h&coinId=\(coin.id)") else { return }
+            let task = URLSession.shared.dataTask(with: url) { data, _, error in
+                guard let data = data, error == nil else { return }
+            
+                do {
+                    let result = try JSONDecoder().decode(CoinPrices.self, from: data)
+                    
+                    DispatchQueue.main.async {
+                        var entries = [ChartDataEntry]()
+                        for group in result.chart {
+                            entries.append(ChartDataEntry(x: group[0], y: group[2]))
+                        }
+                        let set = LineChartDataSet(entries: entries)
+                        set.colors = ChartColorTemplates.liberty()
+                        let data = LineChartData(dataSet: set)
+                        self.lineChart.drawMarkers = false
+                        self.lineChart.data = data
+                        
+                    }
+                    
+                } catch let err {
+                    print(err)
+                }
+            }
+            
+            task.resume()
+        }
+    
+    
+    
+    // MARK: Codes below are NOT working properly...
     @IBAction func dayButtonTapped(_ sender: Any) {
         dayButton.titleLabel?.textColor = .red
         weekButton.titleLabel?.textColor = .systemGray
